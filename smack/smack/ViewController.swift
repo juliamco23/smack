f24587ee7166dc44e04e592dd01dcd54797cdcff
juliam <costajuliam@gmail.com>
2021-03-20 17:07:12 +0000

    first class - n°63

---
+++ smack/smack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  smack
+//
+//  Created by Juliam Costa on 20/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
