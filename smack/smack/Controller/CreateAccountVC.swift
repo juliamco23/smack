30a68201f7843396db615ba60f0dbac67012c572
juliam <costajuliam@gmail.com>
2021-03-26 20:13:43 +0000

    Segue Perform - createAccntBtn

---
+++ smack/smack/Controller/CreateAccountVC.swift
@@ -0,0 +1,17 @@
+//
+//  CreateAccountVC.swift
+//  smack
+//
+//  Created by Juliam Costa on 26/03/2021.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
--- smack/smack/Controller/LoginVC.swift
+++ smack/smack/Controller/LoginVC.swift
@@ -14,9 +14,13 @@ class LoginVC: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    @IBAction func closePressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)    
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func createAccntBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
+    }
     
 }
