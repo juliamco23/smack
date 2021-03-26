adfb457b9bb11ddb2d755821fbfb40f7471fe6f7
juliam <costajuliam@gmail.com>
2021-03-26 18:12:53 +0000

    End of Forth Lesson - LoginVC

---
+++ smack/smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Juliam Costa on 26/03/2021.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)    
+    }
+    
+    
+}
