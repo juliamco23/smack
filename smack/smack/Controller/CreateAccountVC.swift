363308f2d09a28c672a9ccd80f5b940116ee9096
juliam <costajuliam@gmail.com>
2021-03-30 10:25:25 +0100

    create account action buttons

--- smack/smack/Controller/CreateAccountVC.swift
+++ smack/smack/Controller/CreateAccountVC.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    }
+    @IBAction func createAccntPressed(_ sender: Any) {
+    
+    }
+    @IBAction func pickBGColorpressed(_ sender: Any) {
+    }
+    @IBAction func pickAvaterPressed(_ sender: Any) {
     }
     @IBAction func closeBtnPressed(_ sender: Any) {
        performSegue(withIdentifier: UNWIND, sender: nil)
---
+++ smack/smack/Services/AuthService.swift
@@ -0,0 +1,77 @@
+//
+//  AuthService.swift
+//  smack
+//
+//  Created by Juliam Costa on 29/03/2021.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    var isLoggeedIn : Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken: String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+    
+        let lowerCaseEmail = email.lowercased()
+        
+        
+        let header: HTTPHeaders = [
+            "Authorization": "Basic MY-API-KEY",
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        AF.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            
+            switch response.result {
+            case let .success(value):
+                print(value)
+            case let .failure(error):
+                print(error)
+            }
+//
+//            if response.result.error == nil {
+//                completion(true)
+//            } else {
+//                completion(false)
+//                debugPrint(response.result.error as Any)
+        }
+    }
+        
+}
+
